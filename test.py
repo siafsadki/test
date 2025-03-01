b8206ee5cfce3423a5cff53e0d845cc284e36ef3
siafsadki <siaf.sadki@gmail.com>
2025-02-28 16:02:19 -0800

    test4

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-test
-test
-test
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
